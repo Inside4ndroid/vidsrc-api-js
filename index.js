7d9f1706014d6f5a1d8709a499c862e7e539a1c8
Inside4ndroid <inside4ndroid.techsup@gmail.com>
2025-02-15 08:22:17 +0000

    remove unused provider

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { getvidsrc } from "./src/vidsrcpro.js";
-import { getasiaheroku } from "./src/asiaheroku.js";
 
 const port = 3000;
 
@@ -28,25 +27,6 @@ app.get('/vidsrc/:tmdbId', async (req, res) => {
             res.status(200).json(vidsrcresponse);
         } else {
             const vidsrcresponse = await getvidsrc(id);
-            res.status(200).json(vidsrcresponse);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
-});
-
-app.get('/asiaheroku/:tmdbId', async (req, res) => {
-    const id = req.params.tmdbId;
-    const season = req.query.s;
-    const episode = req.query.e;
-
-    try {
-        if (season && episode) {
-            const srcresponse = await getasiaheroku(id, season, episode);
-            res.status(200).json(srcresponse);
-        } else {
-            const vidsrcresponse = await getasiaheroku(id);
             res.status(200).json(vidsrcresponse);
         }
     } catch (error) {
